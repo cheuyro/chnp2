bc3d0865f4b909e948c30b15f8fb2359bb2ab737
cheuyro <gregori123123123@gmail.com>
2019-11-18 14:27:30 +0300

    Измена лога

--- grisha/MyLog.php
+++ grisha/MyLog.php
@@ -7,11 +7,15 @@ class MyLog extends \core\LogAbstract implements \core\LogInterface{
 
 	public static function log($str) {
 	//	if (isset($str))		
-			self::Instance()->log[] = $str; //date('[d-m-Y H:i:s] ').
+			self::Instance()->log[] = date('[d-m-Y H:i:s] ').$str; //date('[d-m-Y H:i:s] ').
 	}
 	
 	public function _write(){
 		echo implode("\n", self::Instance()->log)."\n";
+		if (!is_dir("project/laba3/log")){
+			mkdir("project/laba3/log",0777,true);
+		}
+		file_put_contents("project/laba3/log/log",implode("\n", self::Instance()->log) . PHP_EOL, FILE_APPEND);
 	}
 	
 	public static function write(){
