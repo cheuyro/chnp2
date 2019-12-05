0064ca72bc03fff23c522ff0140abcd13683d0d3
psixxxspb <g5500001@yandex.ru>
2019-12-05 15:30:08 +0300

    Лабораторная работа №9 Сборщик и логи

---
+++ build.php
@@ -0,0 +1,7 @@
+<?php
+
+if (!is_dir('./core'))
+{
+	shell_exec('git clone https://github.com/rok9ru/trpo-core core');
+}
+	file_put_contents('./version', trim(shell_exec('git symbolic-ref --short -q HEAD')));
