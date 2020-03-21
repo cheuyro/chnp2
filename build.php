8299208ee369a9925976a6c0c1570df47181ae47
psixxxspb <g5500001@yandex.ru>
2020-03-21 12:16:29 +0300

    Плановое слияние локалки спустя 4 месяца

--- build.php
+++ build.php
@@ -4,4 +4,5 @@ if (!is_dir('./core'))
 {
 	shell_exec('git clone https://github.com/rok9ru/trpo-core core');
 }
-	file_put_contents('./version', trim(shell_exec('git symbolic-ref --short -q HEAD')));
+
+file_put_contents('./version', trim(shell_exec('git symbolic-ref --short -q HEAD')));
